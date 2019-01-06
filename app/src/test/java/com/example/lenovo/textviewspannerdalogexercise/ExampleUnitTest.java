807c2360d09d2231e7950419ae3426a92f2ee42b
xue <598240754@qq.com>
2019-01-06 20:03:48 +0800

    文件存储方式

--- app/src/test/java/com/example/lenovo/textviewspannerdalogexercise/ExampleUnitTest.java
+++ app/src/test/java/com/example/lenovo/textviewspannerdalogexercise/ExampleUnitTest.java
@@ -10,8 +10,21 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private String testTime = "2018-11-11 22:14:00";
+    private String wrongTimeFormat = "这是错误的时间测试格式";
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testTime() throws Exception {
+        assertEquals("测试结果", TestJunitUnits.dateToStamp(testTime));
+    }
+
+    @Test
+    public void testWrongTime() throws Exception {
+        assertEquals("测试结果", TestJunitUnits.dateToStamp(wrongTimeFormat));
     }
 }
